569c6d203b16fb19763825e7fa208c6543c5134e
Shyla Earl <shyla.earl@gmail.com>
2021-05-07 16:56:57 -0500

    installed required dependancies, updated index.js, created store instance

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(
+    <Provider storeInstance={storeInstance}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
