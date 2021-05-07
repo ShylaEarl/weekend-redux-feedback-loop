07fab2e9af7cb77fa065655f26431475a7c6a087
Shyla Earl <shyla.earl@gmail.com>
2021-05-07 17:35:43 -0500

    installed hashrouter, set up APP component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import './App.css';
+//import { useDispatch, useSelector } from 'react-redux';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+
+//TODO add component imports here for each page
 
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+      </div>
+      <Route>
+        {/* TODO add route path and component here can use <Route exact path="/" component={} />  */}
+      </Route>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,20 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
-import logger from 'redux-logger';
+
+//redux
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
+//TODO add reducers here for each page
+const reducer1 = (state = [], action) => {
+    return state;
+}
 
 const storeInstance = createStore(
     combineReducers({
-
+        reducer1
     }),
-    applyMiddleware(logger),
+    applyMiddleware(logger)
 );
 
 ReactDOM.render(
-    <Provider storeInstance={storeInstance}>
+    <Provider store={storeInstance}>
         <App />
     </Provider>, 
     document.getElementById('root'));
